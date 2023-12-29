46d557311aa14c2ce8e3d9f0b327cc053c32a482
Therohitt <150257140+Therohitt@users.noreply.github.com>
2023-12-29 22:41:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+ 
+ 
+ 
+ const clock =document.getElementById("clock")
+
+//  document.querySelector("#clock") or
+
+
+setInterval (function (){
+
+    let date = new Date()
+
+// console.log(date.toLocaleTimeString());
+clock.innerHTML = date.toLocaleTimeString();
+
+
+
+},  1000)
